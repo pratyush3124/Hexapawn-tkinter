04d7147371ccdad09a97ba1d1989b44aac3c8230
pratyush3124 <connect2pratyush@gmail.com>
2020-04-05 17:29:09 +0530

    initial

---
+++ hexapawn.py
@@ -0,0 +1,150 @@
+from tkinter import Canvas,Tk,PhotoImage, Frame
+
+class Hexapawn(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+
+        self.geometry('375x375')
+        self.configure(background = 'black')
+
+        self.frame = Frame(self,bg = 'red')
+        
+        self.p = [0]*3
+
+        self.board = Board()
+
+        self.wpieces = [0]*3
+        self.bpieces = [0]*3
+
+        # can = Canvas(self, height = 360, width = 360)
+        for i in range(3):
+            self.p[i] = [0]*3
+            self.wpieces[i] = [0]*3
+            self.bpieces[i] = [0]*3
+            for j in range(3):
+                if i == j or i+j == 2:
+                    self.p[i][j] = Canvas(self.frame, bg = 'gray', height = 120, width = 120)
+                else:
+                    self.p[i][j] = Canvas(self.frame, bg = 'white', height = 120, width = 120)
+                self.p[i][j].grid(row = i, column = j)
+
+        self.whitep = PhotoImage(file = 'C:\\Users\\talk2\\OneDrive\\Desktop\\Python\\hexapawn\\whitepawn1.png')
+        self.blackp = PhotoImage(file = 'C:\\Users\\talk2\\OneDrive\\Desktop\\Python\\hexapawn\\blackpawn1.png')
+
+        self.frame.pack()
+
+        for i in range(3):
+            self.bpieces[0][i] = self.p[0][i].create_image(60,60,image = self.blackp)
+
+        for i in range(3):
+            self.wpieces[2][i] = self.p[2][i].create_image(60,60,image = self.whitep)
+
+        
+        self.turn('w')
+
+
+    def turn(self,side):
+        if side == 'w':
+            for i in range(3):
+                for j in range(3):
+                    if self.wpieces[i][j] == 1:
+                        self.p[i][j].bind("<Button-1>", lambda event, r=i, c=j:self.click(r,c))
+                    else:
+                        self.p[i][j].bind("<Button-1>", lambda event:self.clear())
+
+        if side == 'b':
+            for i in range(3):
+                for j in range(3):
+                    if self.bpieces[i][j] == 1:
+                        self.p[i][j].bind("<Button-1>", lambda event, r=i, c=j:self.click(r,c))
+                    else:
+                        self.p[i][j].bind("<Button-1>", lambda event:self.clear())
+
+    def clear(self):
+        for i in range(3):
+            for j in range(3):
+                if i == j or i+j == 2:
+                    self.p[i][j]['bg'] = 'gray'
+                else:
+                    self.p[i][j]['bg'] = 'white'
+
+    def click(self,r,c):
+        self.clear()
+        options = self.board.showmoves(r,c)
+        for i in options:
+            self.p[i[0]][i[1]]['bg'] = 'yellow'
+            self.p[i[0]][i[1]].bind("<Button-1>", lambda event, ro=i[0], co=i[1]: self.move(r,c,ro,co))
+ 
+
+    def move(self,fr,fc,tr,tc):
+        if self.p[tr][tc]['bg'] == 'yellow':
+            if self.wpieces[fr][fc] == 1:
+                self.p[tr][tc].create_image(60,60,image = self.whitep)
+                self.p[fr][fc].delete(self.wpieces[fr][fc])
+                self.clear()
+                self.board.makemove(fr,fc,tr,tc)
+
+            elif self.bpieces[fr][fc] == 1:
+                self.p[tr][tc].create_image(60,60,image = self.blackp)
+                self.p[fr][fc].delete(self.bpieces[fr][fc])
+                self.clear()
+                self.board.makemove(fr,fc,tr,tc)
+
+            for i1 in self.p:
+                for j1 in i1:
+                    j1.bind("<Button-1>", lambda event: self.donothing())
+
+            if self.wpieces[fr][fc] == 1:
+                self.turn('b')
+            elif self.bpieces[fr][fc] == 1:
+                self.turn('w')
+
+        else:
+            self.clear()
+
+    def donothing(self):
+        pass
+
+
+class Board():
+    def __init__(self):
+        self.board = [0]*3
+        for i in range(3):
+            self.board[i] = [0]*3
+
+        for i in range(3):
+            self.board[0][i] = 'b'
+            self.board[2][i] = 'w'
+
+    def showmoves(self,r,c):
+        options = []
+        if self.board[r][c] == 'w':
+            try:
+                if self.board[r-1][c] == 0:
+                    options.append((r-1,c))
+                if self.board[r-1][c-1] == 'b':
+                    options.append((r-1,c-1))
+                if self.board[r-1][c+1] == 'b':
+                    options.append((r-1,c+1))
+            except:
+                pass
+        if self.board[r][c] == 'b':
+            try:
+                if self.board[r+1][c] == 0:
+                    options.append((r+1,c))
+                if self.board[r+1][c-1] == 'w':
+                    options.append((r+1,c-1))
+                if self.board[r+1][c+1] == 'w':
+                    options.append((r+1,c+1))
+            except:
+                pass
+        return options
+
+    def makemove(self,fr,fc,tr,tc):
+        a = self.board[fr][fc]
+        self.board[fr][fc] = 0
+        self.board[tr][tc] = a
+
+
+gameone = Hexapawn()
+gameone.mainloop()
