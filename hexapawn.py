5b20af43dd015d5e02327c29f75ab481b4f93bff
pratyush3124 <connect2pratyush@gmail.com>
2020-04-11 12:35:03 +0530

    ai-random

--- hexapawn.py
+++ hexapawn.py
@@ -1,4 +1,5 @@
 from tkinter import Canvas, Tk, PhotoImage, Frame, messagebox, Button
+import random
 
  
 class Hexapawn(Tk):
@@ -7,20 +8,26 @@ class Hexapawn(Tk):
 
         self.geometry('475x375')
 
-        self.frame = Grid(self)
-        self.frame.grid(row = 0, column = 0)
+        self.frames = [0]
+        self.frames[0] = Grid(self)
+        self.frames[0].grid(row = 0, column = 0)
+
+        self.ai = AI(self)
 
         self.playagainb = Button(self, text = "Play Again", command = lambda:self.playagain())
         self.playagainb.grid(row = 0, column = 1)
 
     def playagain(self):
-        self.frame.destroy()
-        self.frame = Grid(self)
-        self.frame.grid(row = 0, column = 0)
+        self.frames.append(Grid(self))
+        self.frames[len(self.frames)-1].grid(row = 0, column = 0)
+        del self.frames[0]
+        self.ai.refresh()
+
 
 class Grid(Frame):
     def __init__(self, parent):
         Frame.__init__(self,parent)
+
         self.parent = parent
 
         self.p = [0]*3
@@ -53,6 +60,7 @@ class Grid(Frame):
 
         self.wturn = 'w'
         self.nturn = 'b'
+        self.moveno = 1
         self.turn()
 
     def turn(self):
@@ -79,35 +87,75 @@ class Grid(Frame):
             self.p[i[0]][i[1]].bind("<Button-1>", lambda event, ro=i[0], co=i[1]: self.move(r,c,ro,co))
  
     def move(self,fr,fc,tr,tc):
-        if self.p[tr][tc]['bg'] == '#ffe12b':
-            if self.pieces[self.nturn][tr][tc] >= 1:
-                self.p[tr][tc].delete(self.pieces[self.nturn][tr][tc])
-                self.pieces[self.wturn][tr][tc] = self.p[tr][tc].create_image(60,60,image = self.images[self.wturn])
-                self.p[fr][fc].delete(self.pieces[self.wturn][fr][fc])
-                self.pieces[self.nturn][tr][tc] = 0
-            else:
-                self.pieces[self.wturn][tr][tc] = self.p[tr][tc].create_image(60,60,image = self.images[self.wturn])
-                self.p[fr][fc].delete(self.pieces[self.wturn][fr][fc])
-                self.pieces[self.wturn][fr][fc] = 0
-            self.pieces[self.wturn][fr][fc] = 0
+        if self.pieces[self.nturn][tr][tc] >= 1:
+            self.p[tr][tc].delete(self.pieces[self.nturn][tr][tc])
+        self.pieces[self.wturn][tr][tc] = self.p[tr][tc].create_image(60,60,image = self.images[self.wturn])
+        self.p[fr][fc].delete(self.pieces[self.wturn][fr][fc])
+        self.pieces[self.wturn][fr][fc] = 0
 
-            self.clear()
-            self.board.makemove(fr,fc,tr,tc)
+        self.clear()
+        self.board.makemove(fr,fc,tr,tc)
 
-            for i1 in self.p:
-                for j1 in i1:
-                    j1.bind("<Button-1>", lambda event: self.donothing())
+        for i1 in self.p:
+            for j1 in i1:
+                j1.bind("<Button-1>", lambda event: self.donothing())
 
-            a = self.wturn
-            self.wturn = self.nturn
-            self.nturn = a
-            self.turn()
+        b = self.checkwin()
+        if b:
+            return
 
+        a = self.wturn
+        self.wturn = self.nturn
+        self.nturn = a
+        self.moveno += 1
+
+        if self.wturn == 'b':
+            self.parent.ai.turn()
+            # self.turn()
         else:
-            self.clear()
+            self.turn()
 
     def donothing(self):
         pass
+
+    def checkwin(self):
+        if 'w' in self.board.board[0]:
+            messagebox.showinfo('Game Over','White Wins')
+            self.parent.playagain()
+            return True
+        elif 'b' in self.board.board[2]:
+            messagebox.showinfo('Game Over','Black Wins')
+            self.parent.playagain()
+            return True
+
+        t = True
+        s = True
+        for i in range(3):
+            if 'b' in self.board.board[i]:
+                s = False
+            if 'w' in self.board.board[i]:
+                t = False
+        if s:
+            messagebox.showinfo('Game Over','White Wins')
+            self.parent.playagain()
+            return True
+        if t:
+            messagebox.showinfo('Game Over','Black Wins')
+            self.parent.playagain()
+            return True
+
+        a = self.nturn
+        t = True
+        for i in range(3):
+            for j in range(3):
+                if self.board.board[i][j] == a:
+                    if len(self.board.showmoves(i,j)) > 0:
+                        t = False
+        if t:
+            messagebox.showinfo('Game Over','{} Wins'.format(self.wturn))
+            self.parent.playagain()
+            return True
+
 
 class Board():
     def __init__(self, parent): 
@@ -153,42 +201,32 @@ class Board():
         a = self.board[fr][fc]
         self.board[fr][fc] = 0
         self.board[tr][tc] = a
-        self.checkwin()
 
-    def checkwin(self):
-        if 'w' in self.board[0]:
-            messagebox.showinfo('Game Over','White Wins')
-            self.parent.parent.playagain()
-        elif 'b' in self.board[2]:
-            messagebox.showinfo('Game Over','Black Wins')
-            self.parent.parent.playagain()
-        t = True
-        s = True
-        for i in range(3):
-            if 'b' in self.board[i]:
-                s = False
-            if 'w' in self.board[i]:
-                t = False
-        if t:
-            messagebox.showinfo('Game Over','Black Wins')
-            self.parent.parent.playagain()
-        if s:
-            messagebox.showinfo('Game Over','White Wins')
-            self.parent.parent.playagain()
 
-        if self.turn == 'w':
-            a = 'b'
-        else:
-            a = 'w'
-        t = True
+class AI():
+    def __init__(self,parent):
+        self.parent = parent
+        self.hierarchy = {1:None, 3:None, 5:None}
+        self.frame = self.parent.frames[len(self.parent.frames)-1]
+
+    def turn(self):
+        possible = []
         for i in range(3):
             for j in range(3):
-                if self.board[i][j] == a:
-                    if len(self.showmoves(i,j)) > 0:
-                        t = False
-        if t:
-            messagebox.showinfo('Game Over','{} Wins'.format(self.turn))
-            self.parent.parent.playagain()
+                if self.frame.board.board[i][j] == 'b':
+                    possible.append((i,j))
+        subject = random.choice(possible)
+        subjectsoptions = self.frame.board.showmoves(subject[0], subject[1])
+        if subjectsoptions == []:
+            possible.remove(subject)
+            subject = random.choice(possible)
+            subjectsoptions = self.frame.board.showmoves(subject[0], subject[1])
+
+        moved = random.choice(subjectsoptions)
+        self.frame.move(subject[0],subject[1], moved[0], moved[1])
+
+    def refresh(self):
+        self.frame = self.parent.frames[len(self.parent.frames)-1]
 
 
 if __name__ == '__main__':
